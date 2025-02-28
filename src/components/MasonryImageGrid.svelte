61703f2b63c29719e8dc9427e4b8be64811f1dd8
robo-monk <rrobomonk@gmail.com>
2025-02-28 14:09:26 +0100

    feat: add masonry layout for image grid and update project pages
    - Introduce masonry layout using masonry-layout package
    - Update project pages to utilize new layout
    - Add new images and update dependencies in package.json
    - Remove contact page and update about page with new content
    - Refactor MasonryImageGrid component for better responsiveness

--- src/components/MasonryImageGrid.svelte
+++ src/components/MasonryImageGrid.svelte
@@ -1,56 +1,109 @@
 <script lang="ts">
-    import { onMount, tick, type Snippet } from "svelte";
+  import { onMount, tick, type Snippet } from "svelte";
 
-    // @ts-ignore
-    import Bricks from "bricks.js";
-    import type { ProjectData } from "../lib/getProjects";
-    import FullScreenable from "./FullScreenable.svelte";
+  // @ts-ignore
+  //   import Bricks from "bricks.js";
+//   import Masonry from "masonry-layout";
+  import type { ProjectData } from "../lib/getProjects";
+  import FullScreenable from "./FullScreenable.svelte";
 
-    // let { images }: { images: ProjectData["images"] } = $props();
-    let { children }: { children: Snippet } = $props();
+  // let { images }: { images: ProjectData["images"] } = $props();
+  let {
+    children,
+    smColumns = 2,
+    mdColumns = 3,
+    lgColumns = 3,
+  }: {
+    children: Snippet;
+    smColumns?: number;
+    mdColumns?: number;
+    lgColumns?: number;
+  } = $props();
+  let wrapper: HTMLElement;
 
-    let wrapper: HTMLElement;
+  let packing = $state(false);
+//   $effect(() => {
+//     if (wrapper) {
+//       packing = true;
+//       // find [data-masonry-item]
+//       tick().then(() => {
+//         const items = wrapper.querySelectorAll("[data-masonry-item]");
+//         // console.log("found items", items);
+//         if (!items) throw new Error("MasonryImageGrid: container not found");
 
-    $effect(() => {
-        if (wrapper) {
-            // find [data-masonry-item]
-            tick().then(() => {
-                const items = wrapper.querySelectorAll("[data-masonry-item]");
-                // console.log("found items", items);
-                if (!items)
-                    throw new Error("MasonryImageGrid: container not found");
+//         const container = items[0].parentElement;
 
-                const container = items[0].parentElement;
+//         // add resize observer
+//         // const observer = new ResizeObserver(async () => {
+//         //     if (packing) return;
+//         //     console.log("resize from observer");
+//         //     await tick();
+//         //     // requestAnimationFrame(() => {
+//         //     //     instance.pack();
+//         //     // });
+//         // });
 
-                // add resize observer
-                const observer = new ResizeObserver(async () => {
-                    console.log("resize from observer");
-                    await tick();
-                    instance.pack();
-                });
+//         Array.from(wrapper.querySelectorAll("img")).forEach((child) => {
+//           console.log("child", child.nodeName);
+//           child.onload = async (data) => {
+//             console.log("child image has been loaded", data);
+//             console.log("child image has been loaded");
+//             await tick();
+//             await new Promise((resolve) => setTimeout(resolve, 1000));
+//             instance.update()
+//           };
+//         });
 
-                Array.from(items).forEach((child) => {
-                    console.log("child", child);
-                    observer.observe(child);
-                });
+//         const sizes = [
+//           { columns: smColumns, gutter: 10 },
+//           { mq: "768px", columns: mdColumns, gutter: 25 },
+//           { mq: "1024px", columns: lgColumns, gutter: 20 },
+//         ];
+//         const instance = new Masonry(container, {
+//           itemSelector: "[data-masonry-item]",
+//           columnWidth: 200,
+//           horizontalOrder: true,
+//         //   fitWidth: true,
+//         //   percentPosition: true,
+//         //   columnWidth: ".grid-sizer",
+//         //   percentPosition: true,
+//           gutter: 10,
+//         });
 
-                const sizes = [
-                    { columns: 2, gutter: 10 },
-                    { mq: "768px", columns: 2, gutter: 25 },
-                    { mq: "1024px", columns: 3, gutter: 20 },
-                ];
-                const instance = new Bricks({
-                    container,
-                    packed: "packed",
-                    sizes,
-                });
-
-                instance.resize(true).pack();
-            });
-        }
-    });
+//         instance.resize(true).pack();
+//         packing = false;
+//       });
+//     }
+//   });
 </script>
 
-<div bind:this={wrapper} class="w-full h-full relative">
+<!-- <div bind:this={wrapper} class="w-full h-full relative grid">
+  {@render children()}
+</div> -->
+
+<style lang="scss">
+    #masonry-grid {
+        display: grid;
+        grid-template-rows: auto;
+        grid-template-columns: repeat(var(--smColumns), 1fr);
+        gap: 1rem;
+
+        :global(img) {
+            margin: 0;
+        }
+    }
+    @media (min-width: 768px) {
+        #masonry-grid {
+            grid-template-columns: repeat(var(--mdColumns), 1fr);
+        }
+    }
+    @media (min-width: 1024px) {
+        #masonry-grid {
+            grid-template-columns: repeat(var(--lgColumns), 1fr);
+        }
+    }
+</style>
+
+<div id="masonry-grid" class="w-full h-full relative" style={`--smColumns: ${smColumns}; --mdColumns: ${mdColumns}; --lgColumns: ${lgColumns};`}>
     {@render children()}
 </div>
--- src/projects/illustrations/life-drawings/description.md
+++ src/projects/illustrations/life-drawings/description.md
@@ -6,6 +6,8 @@ Throughout my year of studying illustration, I followed a weekly course about th
 
 Below are some of my favorite works that I made during this course. They include still lifes, abstract work and figure studies of (nude) models. 
 
-Medium: mixed media; ink, acrylic, gouache, ecoline, charcoal, pastels and more
-Size: varies; around 50 x 70 cm
+<Images images="1.jpg,cover.jpg" height="400px" width="300px" />
+
+**Medium:** mixed media; ink, acrylic, gouache, ecoline, charcoal, pastels and more <br>
+**Size:** varies; around 50 x 70 cm
 
